f65674520bdc57d390fb2bfb94e4495637cd6871
Maik125 <miguelfin43@gmail.com>
2024-11-28 17:16:00 -0600

    se añade lista de links creados con bootstrap x4 y se limitan a 5

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def create_DB():
 def home():
     con = conn()
     cursor = con.cursor()
-    cursor.execute("SELECT * FROM links")
+    cursor.execute("SELECT TOP 5 * FROM links ORDER BY id DESC")
     rows = cursor.fetchall()
 
 
